b4f9bb31de7b9b2d7f8e7783c11c180d3c4e4e51
matheusthiesen <matheusreis314@gmail.com>
2022-03-07 04:19:15 -0300

    Configurando testes

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+// yarn add jest jest-dom @testing-library/jest-dom @testing-library/react babel-jest -D
+
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTest.ts"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  testEnvironment: "jsdom",
+};
